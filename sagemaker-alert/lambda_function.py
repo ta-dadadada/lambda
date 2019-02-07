7e4748bf5c7c08e81018c830de307fdbdce35b8d
ta-dadadada <94or2.t@gmail.com>
2019-02-08 01:23:54 +0900

    sagemaker-alertの見た目を調整

---
+++ sagemaker-alert/lambda_function.py
@@ -0,0 +1,118 @@
+import json
+import boto3
+import os
+import requests
+from datetime import timezone, timedelta
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Slack関連の設定を環境変数から読み込む
+SLACK_POST_URL = os.environ['slackPostURL']
+SLACK_CHANNEL = os.environ['slackChannel']
+
+# JSTを定義する
+JST = timezone(timedelta(hours=9), 'JST')
+
+
+def lambda_handler(event, context):
+    # SageMakerが対応してるリージョン一覧を取得
+    regions = boto3.Session().get_available_regions('sagemaker')
+
+    # リージョンごとに実行
+    notebooks_for_regions = {}
+    endpoints_for_regions = {}
+
+    for region in regions:
+        sm = boto3.client('sagemaker', region_name=region)
+
+        # 起動中(InService)のインスタンスを取得する
+        notebooks = sm.list_notebook_instances(
+            StatusEquals='InService'
+        )['NotebookInstances']
+        if len(notebooks) > 0:
+            notebooks_for_regions[region] = notebooks
+
+        # 起動中のエンドポイントを取得する
+        endpoints = sm.list_endpoints(
+            StatusEquals='InService'
+        )['Endpoints']
+        if len(endpoints) > 0:
+            endpoints_for_regions[region] = endpoints
+
+    # ノートブックインスタンスとエンドポイントのそれぞれの表示内容を作成する
+    field_map = {
+        'notebook': create_view_for_regions(notebooks_for_regions, create_notebook_field),
+        'endpoint': create_view_for_regions(endpoints_for_regions, create_endpoint_field),
+    }
+    color_map = {
+        'notebook': '#439FE0',
+        'endpoint': 'good',
+    }
+    # Slackにメッセージを送る
+    atts = []
+    for key, fields in field_map.items():
+        if not fields:
+            text = f'起動している {key} はないみたい'
+        else:
+            text = f'起動中の {key} 一覧'
+        atts.append({
+            'pretext': text,  # 内容
+            'fields': fields,
+            'color': color_map.get(key, 'warning')
+        })
+    send_slack_message('SageMaker使用状況', attachments=atts)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('done')
+    }
+
+
+def create_view_for_regions(dic, func):
+    # リージョン毎のデータ一覧用の表示を作る
+    fields = []
+    for region, data_list in dic.items():
+        if len(data_list) == 0:
+            continue
+        for data in data_list:
+            fields.append(func(data, region))
+    return fields
+
+
+def create_field(name: str, modified: str, region: str, short: bool = False) -> dict:
+    return {'title': name, 'value': f'{region}: {modified} から起動中', 'short': short}
+
+
+def create_notebook_field(data: dict, region: str) -> dict:
+    name = data['NotebookInstanceName']
+    modified = data['LastModifiedTime'].astimezone(JST).strftime('%Y/%m/%d %H:%M:%S')
+    return create_field(name, modified, region)
+
+
+def create_endpoint_field(data: dict, region: str) -> dict:
+    name = data['EndpointName']
+    created = data['CreationTime'].astimezone(JST).strftime('%Y/%m/%d %H:%M:%S')
+    return create_field(name, created, region)
+
+
+def send_slack_message(text: str, attachments: list = None):
+    # Slackにメッセージを送信する
+
+    if not attachments:
+        data = {
+            'channel': SLACK_CHANNEL,  # 送信先チャンネル
+            'text': text,  # 内容
+        }
+    else:
+        data = {
+            'channel': SLACK_CHANNEL,  # 送信先チャンネル
+            'text': text,
+            'attachments': attachments,
+        }
+    try:
+        req = requests.post(SLACK_POST_URL, data=json.dumps(data))
+        logger.info('Message posted to %s', data['channel'])
+    except requests.exceptions.RequestException as e:
+        logger.error('Request failed: %s', e)
